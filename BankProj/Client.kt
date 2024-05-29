ebd927c7c1e46655c48168fb31d95f38e402374a
MrAladawee <91622707+MrAladawee@users.noreply.github.com>
2024-05-30 00:16:43 +0300

    Update Client.kt

--- BankProj/Client.kt
+++ BankProj/Client.kt
@@ -1,34 +1,29 @@
 import java.io.PrintWriter
 import java.net.Socket
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
 
 class Client(
     val host: String = "localhost",
     val port: Int = 8080) {
 
-    fun sendRequest(request: String) {
-        var socket: Socket? = null
+    fun sendRequest(request: String){
+        var socket : Socket? = null
         try {
             socket = Socket(host, port)
-            var pw = PrintWriter(socket.getOutputStream())
+
+            var pw = PrintWriter(socket.getOutputStream(), true)
             pw.println(request)
-            pw.flush()
 
             var br = socket.getInputStream().bufferedReader()
-            var line: String?
-            while (br.readLine().also { line = it } != null) {
-                println(line)
-            }
+            println("Response from server: ${br.readLine()}")
 
             pw.close()
             br.close()
-        } catch (e: Exception) {
+        }
+        catch (e: Exception){
             println(e.message)
-        } finally {
+        }
+        finally{
             socket?.close()
         }
     }
-
 }
